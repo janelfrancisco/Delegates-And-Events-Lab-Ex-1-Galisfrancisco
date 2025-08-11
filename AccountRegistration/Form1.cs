ea661ee27a09b1964cc840de4d780f305889e240
janelfrancisco <janelfrancisco2122@gmail.com>
2025-08-11 19:43:16 +0800

    UI CREATED

--- AccountRegistration/Form1.cs
+++ AccountRegistration/Form1.cs
@@ -16,5 +16,10 @@ namespace AccountRegistration
         {
             InitializeComponent();
         }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
